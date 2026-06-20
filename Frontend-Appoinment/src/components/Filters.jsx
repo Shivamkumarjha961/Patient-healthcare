8765a4520b8e56216f64c36598ebeaa974f0aa4b
Shivam kumar jha <shivamkumarjha961@gmail.com>
2026-06-20 10:51:34 +0530

    Optimize doctor profile images to unique WebP, align assets folder structure, and add Orthopedic filter option

--- Frontend-Appoinment/src/components/Filters.jsx
+++ Frontend-Appoinment/src/components/Filters.jsx
@@ -1,7 +1,7 @@
 export default function Filters({spec, setSpec, sort, setSort, minExp, setMinExp})
 { return ( <div className="flex flex-wrap gap-3 items-center"> <select value={spec} onChange={e=>setSpec(e.target.value)} className="border p-2 rounded">
      <option value=''>All Specializations</option> <option>Cardiologist</option><option>Dentist</option>
-     <option>Dermatologist</option><option>Pediatrician</option><option>ENT</option> </select>
+     <option>Dermatologist</option><option>Pediatrician</option><option>ENT</option><option>Orthopedic</option> </select>
       <select value={sort} onChange={e=>setSort(e.target.value)} className="border p-2 rounded">
          <option value='rating'>Sort: Rating</option> 
          <option value='fees'>Sort: Fees</option>
